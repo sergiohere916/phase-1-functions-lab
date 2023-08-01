be74678d2b5589b60cd9351d80a46d941bb1d47f
Sergio Heredia <sergioheredia1996@gmail.com>
2023-07-31 20:08:01 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    let blocks = Math.abs(someValue - 42);
+    return blocks; 
+  }
+function distanceFromHqInFeet(someValue) {
+    let currentDistance = distanceFromHqInBlocks(someValue);
+    let feetDistance = currentDistance * 264;
+    return feetDistance; 
+}
+function distanceTravelledInFeet(start, end) {
+    let blockDistance = Math.abs(end - start);
+    let feetDistance = blockDistance *264;
+    return feetDistance;
+}
+function calculatesFarePrice(start, destination) {
+    let feetDistance = distanceTravelledInFeet(start, destination);
+    if (feetDistance <= 400) {
+        return 0;
+    } else if (feetDistance > 400 && feetDistance <= 2000) {
+        return (feetDistance -400) * .02;
+    } else if (feetDistance > 2000 && feetDistance <= 2500) {
+        return 25;
+    } else if (feetDistance > 2500) {
+        return "cannot travel that far";
+    }
+
+}
